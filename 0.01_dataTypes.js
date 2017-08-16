04e68e065aa5461c4bd955ca781c93f8c436af57
Isaac Ludlow <isaac@theludlows.org>
2017-08-16 13:13:15 -0400

    Added Data Types

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,25 @@
+//Data types
+
+//Strings
+
+"This is a string";
+
+"This is also a string";
+
+"It's a wonferful day";
+
+
+console.log('Couldn\'t things get any better');
+
+console.log( 'Thomas said "I think grass is green!"'.toUpperCase())
+//Used to modify consumer data, such as "yes" to "YES", so the the computer can read it without errors
+
+console.log("IsaAc lUdLow".toLowerCase().blink())
+
+
+console.log("Ho" + "Home"[2].toUpperCase() + "e")
+
+//Numbers
+
+
+//Booleans
